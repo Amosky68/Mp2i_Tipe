310633aac865ab621b0229dfe0d7de286dee695e
Amosky <amoroussel05@gmail.com>
2024-03-24 12:14:00 +0100

    Working Instancing with matices

--- InstancingTest.cs
+++ InstancingTest.cs
@@ -47,18 +47,39 @@ namespace OpenGlTIPE
         protected unsafe override void LoadContent() {
 
             string vertexShader = @"#version 330 core
-                                    layout (location = 0) in vec2 aPosition;
+                                    layout (location = 0) in vec2 aVertsPosition;
                                     layout (location = 1) in vec3 aColor;
-                                    layout (location = 2) in mat4 aModel;
+                                    layout (location = 2) in mat4 aMatrix;
+
+
+
+                                    //layout (location = 6) in vec2 aShapePos;
+
+
                                     out vec4 vertexColor;
     
                                     uniform mat4 projection;
 
+                                    mat4 aMat4 = mat4(1.0, 0.0, 0.0, 0.0,  // 1. column
+                                                      0.0, 1.0, 0.0, 0.0,  // 2. column
+                                                      0.0, 0.0, 1.0, 0.0,  // 3. column
+                                                      0.0, 0.0, 0.0, 1.0); // 4. column
+
+
 
                                     void main() 
                                     {
-                                        vertexColor = vec4(aColor.rgb, 1.0);
-                                        gl_Position = projection * aModel * vec4(aPosition.xy + aOffset, 0, 1.0);
+                                        int coll = 3;
+                                        int line = 3;
+                                        float divider = 1.0;
+                                        mat4 testMatrix = aMatrix;
+
+                                        vec3 collTst = vec3(testMatrix[coll][0],testMatrix[coll][1],testMatrix[coll][2]);
+                                        vec3 lineTst = vec3(testMatrix[0][line],testMatrix[1][line],testMatrix[2][line]) * divider;
+                                        //vec3 collone = c4.xyz;
+
+                                        vertexColor = vec4(aColor.xyz, 1.0);
+                                        gl_Position = projection * aMatrix * vec4(aVertsPosition.xy, 0, 1.0);
                                     }";
 
             string fragmentShader = @"#version 330 core
@@ -85,7 +106,7 @@ namespace OpenGlTIPE
             {
                  1f, 0f, 0f,
                  0f, 1f, 0f,
-                 0f, 0f, 1f,
+                 1f, 0f, 1f,
                  0f, 1f, 0f,
                  0f, 1f, 1f,
                  1f, 0f, 1f,
@@ -94,33 +115,39 @@ namespace OpenGlTIPE
 
             vao = glGenVertexArray();
             glBindVertexArray(vao);
+            uint InstanceVBo;
 
             // verts VBO :
-            uint InstanceVBo = glGenBuffer();
+            
+            InstanceVBo = glGenBuffer();
             glBindBuffer(GL_ARRAY_BUFFER, InstanceVBo);
             fixed (float* v = &cubeVertices[0])
             {
-                glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 2 * cubeVertices.Length, v, GL_STATIC_DRAW);
+                glBufferData(GL_ARRAY_BUFFER, sizeof(float) * cubeVertices.Length, v, GL_STATIC_DRAW);
             }
             glVertexAttribPointer(0, 2, GL_FLOAT, false, sizeof(float) * 2, (void*)0);
             glEnableVertexAttribArray(0);
 
-
             // Color VBO :
             InstanceVBo = glGenBuffer();
             glBindBuffer(GL_ARRAY_BUFFER, InstanceVBo);
-            fixed (float* v = &cubeColors[0])
+            fixed (float* c = &cubeColors[0])
             {
-                glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 2 * cubeVertices.Length, v, GL_STATIC_DRAW);
+                glBufferData(GL_ARRAY_BUFFER, sizeof(float) * cubeColors.Length, c, GL_STATIC_DRAW);
             }
             glVertexAttribPointer(1, 3, GL_FLOAT, false, sizeof(float) * 3, (void*)0);
             glEnableVertexAttribArray(1);
 
 
+
+
+
             objectsPos = new List<Vector2>();
             int cubeSize = 10;
-            for (int x = -cubeSize/2; x < cubeSize/2; x++){
-                for (int y = -cubeSize / 2; y < cubeSize / 2; y++) {
+            for (int x = -cubeSize / 2; x < cubeSize / 2; x++)
+            {
+                for (int y = -cubeSize / 2; y < cubeSize / 2; y++)
+                {
                     objectsPos.Add(new Vector2(x, y));
                 }
             }
@@ -128,37 +155,80 @@ namespace OpenGlTIPE
             float[] instancesMats = new float[objectsPos.Count * 16];
 
 
+            /*
+            // positions 
+            InstanceVBo = glGenBuffer();
+            glBindBuffer(GL_ARRAY_BUFFER, InstanceVBo);
+            float[] posArry = new float[objectsPos.Count * 2];
+            for (int s = 0; s < objectsPos.Count; s++) { posArry[s * 2] = objectsPos[s].X; posArry[s * 2 + 1] = objectsPos[s].Y; }
+
+
+            fixed (float* c = &posArry[0])
+            {
+                glBufferData(GL_ARRAY_BUFFER, sizeof(float) * cubeColors.Length, c, GL_STATIC_DRAW);
+            }
+            glVertexAttribPointer(6, 2, GL_FLOAT, false, sizeof(float) * 2, (void*)0);
+            glVertexAttribDivisor(6, 1);
+            glEnableVertexAttribArray(6);
+            */
+            
             int i = 0;
             foreach(Vector2 pos in objectsPos)
             {
-                Matrix4x4 trans = Matrix4x4.CreateTranslation(pos.X, pos.Y, 0);
+                Matrix4x4 trans = Matrix4x4.CreateTranslation(pos.X, pos.Y, 0); //CreateTranslation(pos.X, pos.Y, 0);
+                Debug.WriteLine("trans : " + trans);
                 float[] matvalues = Shader.GetMatrix4x4Values(trans);
                 for (int j = 0;  j < 16; j++) {
                     instancesMats[i*16+j] = matvalues[j];
                 }
                 i++;
             }
+            Debug.WriteLine("[{0}]", string.Join(", ", instancesMats));
 
 
 
+
+            glBindVertexArray(vao);
 
             // Bind the Model Buffer
-            InstanceVBo = glGenBuffer();
-            glBindBuffer(GL_ARRAY_BUFFER, InstanceVBo);
+            uint MatrixBuffer = glGenBuffer();
+            glBindBuffer(GL_ARRAY_BUFFER, MatrixBuffer);
+            fixed (float* v = &instancesMats[0])  {
+                glBufferData(GL_ARRAY_BUFFER, sizeof(float) * instancesMats.Length, v, GL_STATIC_DRAW);
+            }
 
+            glVertexAttribPointer(2, 4, GL_FLOAT, false, sizeof(float) * 16, (void*)(0 * sizeof(float)));
+            glVertexAttribPointer(3, 4, GL_FLOAT, false, sizeof(float) * 16, (void*)(4 * sizeof(float)));
+            glVertexAttribPointer(4, 4, GL_FLOAT, false, sizeof(float) * 16, (void*)(8 * sizeof(float)));
+            glVertexAttribPointer(5, 4, GL_FLOAT, false, sizeof(float) * 16, (void*)(12 * sizeof(float)));
+
+            glVertexAttribDivisor(2, 1);
+            glVertexAttribDivisor(3, 1);
+            glVertexAttribDivisor(4, 1);
+            glVertexAttribDivisor(5, 1);
+
+            glEnableVertexAttribArray(2);
+            glEnableVertexAttribArray(3);
+            glEnableVertexAttribArray(4);
+            glEnableVertexAttribArray(5);
+
+
+            //glEnableVertexAttribArray(2-3-4-5);
+
+
+            /*
             for (uint bI = 0; bI < 4; bI++)
             {
-                uint localId = InstanceVBo + bI;
-                glVertexAttribPointer(localId, 4, GL_FLOAT, false, sizeof(float) * 16, (void*)(i * 4));
-                glVertexAttribDivisor(localId, 1);
+                uint localId = 2 + bI;
                 glEnableVertexAttribArray(localId);
+                glVertexAttribPointer(localId, 4, GL_FLOAT, false, sizeof(float) * 16, (void*)(i * 4 * sizeof(float)));
+                glVertexAttribDivisor(localId, 1);
             }
 
             fixed (float* v = &instancesMats[0])
             {
                 glBufferData(GL_ARRAY_BUFFER, sizeof(float) * instancesMats.Length, v, GL_STATIC_DRAW);
-            }
-
+            }*/
 
 
 
@@ -182,15 +252,13 @@ namespace OpenGlTIPE
             glClear(GL_COLOR_BUFFER_BIT);
 
             shader.Use();
-            camera.zoom = .1f;
+            camera.zoom = 100.1f;
             shader.SetMatrix4x4("projection", camera.GetProjectionMatrix());
 
 
 
             glBindVertexArray(vao);
-            glDrawArrays(GL_TRIANGLES, 0, 6);
-            glBindVertexArray(0); 
-            Debug.WriteLine("fps : " + 1 / SimTime.DeltaTime);
+            //Debug.WriteLine("fps : " + 1 / SimTime.DeltaTime);
 
 
             // Instancing objects
@@ -198,10 +266,11 @@ namespace OpenGlTIPE
 
             double t1 = Glfw.Time;
 
-            glDrawArraysInstanced(GL_TRIANGLES, 0, 6, 0);
+            glDrawArraysInstanced(GL_TRIANGLES, 0, 6, 50);
 
             Glfw.SwapBuffers(DisplayManager.Window);
-            Debug.WriteLine("Image drawn in " + (Glfw.Time - t1) + " seconds (" + 1f / (Glfw.Time - t1) + " calls/secs)");
+            //Debug.WriteLine("Image drawn in " + (Glfw.Time - t1) + " seconds (" + 1f / (Glfw.Time - t1) + " calls/secs)");
+            glBindVertexArray(0);
 
         }
     } 
